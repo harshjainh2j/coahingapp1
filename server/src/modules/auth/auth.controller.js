320573d0df00f4c1fac6f1fdacc6627ed926ebb0
Harsh Jain <harshjainh2j@users.noreply.github.com>
2026-05-08 16:32:19 +0530

    login fix

--- server/src/modules/auth/auth.controller.js
+++ server/src/modules/auth/auth.controller.js
@@ -44,8 +44,8 @@ const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true, // required for sameSite: 'none'
+      sameSite: 'none', // allows cross-origin cookies
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     });
 
